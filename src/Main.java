6e33257c6ab731f76625380a6e514a982d23845c
Kaspis <kasparas.ziuraitis@gmail.com>
2024-03-26 12:01:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public static String formatName(String name){
+        return name;//suformatuotas Namecase
+
+//        System.out.println (assertEquals(formatName("NAGLIS"),"Naglis"));
+    }
+
+
+
+
+    public static boolean assertEquals(String actual, String expected){
+        return actual.equals(expected);
+    }
+
+
+}
---
+++ src/Name.java
@@ -0,0 +1,22 @@
+public class Name {
+
+    private String name;
+    private boolean assertEquals;
+
+
+public void seFormatName(String name) {
+    this.name = name;
+}
+public String getFormatName(){
+    return this.name;
+}
+public void setAssertEquals(Boolean assertEquals) {
+    this.assertEquals = assertEquals;
+}
+public boolean getAssertEquals(){
+    return this.assertEquals ;
+}
+
+
+
+}
