b06f05847ff2346b91b20c6f85964afc26c4f131
Kaspis <kasparas.ziuraitis@gmail.com>
2024-03-26 16:54:38 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(assertEquals( formatName("vaida"), "Vaida"));
+        System.out.println(assertEquals( formatName("VAIDA"), "Vaida"));
+        System.out.println(assertEquals( formatName("vAIDA"), "Vaida"));//overlap
+        System.out.println(assertEquals( formatName("  Vaida"), "Vaida"));
+        System.out.println(assertEquals( formatName("Vaida  "), "Vaida"));
+        System.out.println(assertEquals( formatName("Vaida"), "Vaida"));
+    }
+    public static String formatName(String name) {
+        //loose the spaces here
+        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+
+        return name;
+    }
+
+    public static boolean assertEquals(String actual, String expected) {
+       return actual.equals(expected);
     }
 
 
-    public static String formatName(String name){
-        return name;//suformatuotas Namecase
-
-//        System.out.println (assertEquals(formatName("NAGLIS"),"Naglis"));
-    }
-
-
-
-
-    public static boolean assertEquals(String actual, String expected){
-        return actual.equals(expected);
-    }
 
 
 }
+
+
+
--- src/Name.java
+++ src/Name.java
@@ -1,22 +1,25 @@
-public class Name {
-
-    private String name;
-    private boolean assertEquals;
-
-
-public void seFormatName(String name) {
-    this.name = name;
-}
-public String getFormatName(){
-    return this.name;
-}
-public void setAssertEquals(Boolean assertEquals) {
-    this.assertEquals = assertEquals;
-}
-public boolean getAssertEquals(){
-    return this.assertEquals ;
-}
-
-
-
-}
+//public class Name {
+//
+//    private String name;
+//    private boolean assertEquals;
+//
+//    public Name(){
+//
+//    }
+//public void seFormatName(String name) {
+//    this.name = name;
+//}
+//
+//    public static String getFormatName(String name) {
+//        return name.toUpperCase().charAt(0) + name.substring(1).toLowerCase();
+//    }
+//public void setAssertEquals(Boolean assertEquals) {
+//    this.assertEquals = assertEquals;
+//}
+//public boolean getAssertEquals(){
+//    return this.assertEquals ;
+//}
+//
+//
+//
+//}
